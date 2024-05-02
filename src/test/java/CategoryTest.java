0260ecef16d6387f665582ab769bb5810bb2128b
MdShihabAhmed <shihab088068@gmail.com>
2024-05-02 11:42:33 +0600

    changes to CategoryTest

--- src/test/java/CategoryTest.java
+++ src/test/java/CategoryTest.java
@@ -7,4 +7,14 @@ public class CategoryTest {
         Category category = new Category("exampleCategory");
         assert("exampleCategory".equals(category.getMovieCategory()));
     }
+
+    @Test
+    void testAddMovie(){
+        Category category = new Category("exampleCategory");
+        category.addMovie(new Movie(new Title("exampleTitle")));
+        for(Movie movie: category.getMovies()){
+            assert movie.getTitle().getMovieTitle().equals("exampleTitle");
+        }
+
+    }
 }
