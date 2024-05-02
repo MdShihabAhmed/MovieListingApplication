24450bba04688e224242f81d5df4dad3ada423f7
MdShihabAhmed <shihab088068@gmail.com>
2024-05-02 12:01:57 +0600

    added category list to Movie

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -3,11 +3,13 @@ import java.util.ArrayList;
 public class Movie {
     private Title title;
     private ArrayList<Cast> casts;
+    private ArrayList<Category> categories;
 
 
     public Movie(Title title){
         this.title=title;
         casts = new ArrayList<>();
+        categories = new ArrayList<>();
     }
 
     public Title getTitle() {
@@ -20,6 +22,14 @@ public class Movie {
 
     public ArrayList<Cast> getCasts(){
         return casts;
+    }
+
+    public void addCategory(Category category){
+        categories.add(category);
+    }
+
+    public ArrayList<Category> getCategories(){
+        return categories;
     }
 
 }
