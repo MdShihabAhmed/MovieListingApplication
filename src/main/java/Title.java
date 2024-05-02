fb009ec1643c0d0b35c94434826abb4f92f45c36
MdShihabAhmed <shihab088068@gmail.com>
2024-05-02 11:09:17 +0600

    Title done

---
+++ src/main/java/Title.java
@@ -0,0 +1,12 @@
+public class Title {
+    private String movieTitle;
+
+    public Title(String movieTitle){
+        this.movieTitle = movieTitle;
+    }
+
+    public String getMovieTitle(){
+        return movieTitle;
+    }
+
+}
