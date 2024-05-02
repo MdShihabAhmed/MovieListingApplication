78acd56ece625848c0400de82fe7b15e2cd3ff43
MdShihabAhmed <shihab088068@gmail.com>
2024-05-02 11:47:44 +0600

    changes to Category

--- src/main/java/Category.java
+++ src/main/java/Category.java
@@ -1,11 +1,23 @@
+import java.util.ArrayList;
+
 public class Category {
     private String movieCategory;
+    private ArrayList<Movie> movies;
 
     public Category(String movieCategory){
         this.movieCategory = movieCategory;
+        movies = new ArrayList<>();
     }
 
     public String getMovieCategory(){
         return movieCategory;
+    }
+
+    public void addMovie(Movie movie){
+        movies.add(movie);
+    }
+
+    public ArrayList<Movie> getMovies(){
+        return movies;
     }
 }
