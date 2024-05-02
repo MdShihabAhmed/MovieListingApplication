c8c4636762e52f3fbab93dd65e689490bd2ab96d
MdShihabAhmed <shihab088068@gmail.com>
2024-05-02 11:17:23 +0600

    changed test for Movie

--- src/test/java/MovieTest.java
+++ src/test/java/MovieTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class MovieTest {
-    Movie movie = new Movie();
+    Movie movie = new Movie(new Title("exampleTitle"));
 
     @Test
     void testGetTitle() {
