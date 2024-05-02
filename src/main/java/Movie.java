855ffda255f83e580eb760ec4ecfca7db9a6411f
MdShihabAhmed <shihab088068@gmail.com>
2024-05-02 11:58:48 +0600

    added cast list to movie

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -1,11 +1,25 @@
+import java.util.ArrayList;
+
 public class Movie {
     private Title title;
+    private ArrayList<Cast> casts;
+
 
     public Movie(Title title){
         this.title=title;
+        casts = new ArrayList<>();
     }
 
     public Title getTitle() {
         return title;
     }
+
+    public void addCast(Cast cast){
+        casts.add(cast);
+    }
+
+    public ArrayList<Cast> getCasts(){
+        return casts;
+    }
+
 }
