60fe5a3d8e8e1302767ebaf75684853993142b16
MdShihabAhmed <shihab088068@gmail.com>
2024-05-02 11:18:23 +0600

    changed Movie

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -1,7 +1,15 @@
 public class Movie {
     private Title title;
 
-    public Movie(){
+    public Movie(Title title){
+        this.title=title;
+    }
 
+    public Title getTitle() {
+        return title;
+    }
+
+    public void setTitle(Title title) {
+        this.title = title;
     }
 }
